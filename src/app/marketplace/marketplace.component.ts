f8e28dfde8624d22270a2aba922aee3e86b04a3c
Alex Ficklin and Ron Craig <alex+ron@>
2018-04-02 16:30:34 -0700

    add album-detail files to include services

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,23 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
-export class MarketplaceComponent {
-  constructor(private router: Router) {}
 
-  albums: Album[] = [
-    new Album("Parlament's Best Hits", "Parlament", "The best songs from Parlament during mid 70s.", 1),
-    new Album("The Miseducation of Laren Hill", "Laren Hill", "Laren Hill's groundbreaking intro album.", 2),
-    new Album("What's Goin On", "Marvin Gaye", "Marvin Gaye's political and environmental album stating the conditions in the mid 70s.", 3),
-    new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
-       "Second studio album by the English-American band, released in 1967.", 4)
+export class MarketplaceComponent implements OnInit {
+  albums: Album[];
 
-  ];
+  constructor(private router: Router, private albumService: AlbumService) {}
+
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
+  }
 
   goToDetailPage(clickedAlbum: Album) {
     this.router.navigate(['albums', clickedAlbum.id]);
---
+++ src/app/mock-album.ts
@@ -0,0 +1,10 @@
+import { Album } from './album.model';
+
+export const ALBUMS: Album[] = [
+  new Album("Parlament's Best Hits", "Parlament", "The best songs from Parlament during mid 70s.", 1),
+  new Album("The Miseducation of Lauryn Hill", "Miss Lauryn Hill", "Miss Lauryn Hill's groundbreaking album.", 2),
+  new Album("What's Goin On", "Marvin Gaye", "Marvin Gaye's political and environmental album stating the conditions in the mid 70s.", 3),
+  new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
+     "Second studio album by the English-American band, released in 1967.", 4)
+
+];
