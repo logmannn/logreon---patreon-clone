967dd3fed18247014c67ea872346c70e8f62904d
Logan Tanous and Ron Craig <logan@>
2018-04-04 09:22:15 -0700

    add updating and deleting features

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -23,12 +23,16 @@ export class AlbumService {
     return this.database.object('albums/' + albumId);
   }
 
-  updateAlbum(localUpdatedAlbum){
+  updateAlbum(localUpdatedAlbum) {
     let albumEntryInFirebase = this.getAlbumById(localUpdatedAlbum.$key);
     albumEntryInFirebase.update({title: localUpdatedAlbum.title,
                                 artist: localUpdatedAlbum.artist,
                                 description: localUpdatedAlbum.description});
   }
 
+  deleteAlbum(localAlbumToDelete) {
+    let albumEntryInFirebase = this.getAlbumById(localAlbumToDelete.$key);
+    albumEntryInFirebase.remove();
+  }
 
 }
